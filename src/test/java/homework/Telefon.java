7c082826f71469cbfdf5f6be5b588b458dba9b4b
Petronela-lab <you@example.com>
2025-05-25 21:17:13 +0300

    add homework request

---
+++ src/test/java/homework/Telefon.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class Telefon {
+    // De facut un exempli oop asemanator cu exemplul din cursș
+    // Trebuie sa folosim :
+    // mostenire;
+    // incapsulare;
+    // polimorfism;
+    //abstractizare(clase abstracte si interfete);
+}
